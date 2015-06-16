6e7dc97fc715a88074d8010ae5939e8eba5faacf
Dmitry Vyal <akamaus@gmail.com>
2015-06-16 18:13:24 +0300

    crude client supporting switching on and off

--- client.sh
+++ client.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-echo "hello" >  /tmp/led_input
-echo "world" >  /tmp/led_input
+SERV_INPUT="/tmp/led_input"
+SERV_OUTPUT="/tmp/led_output"
+
+case $1 in
+    "get-led-state")
+        echo "get-led-state" > $SERV_INPUT
+        cat $SERV_OUTPUT | grep OK | cut -d \  -f 2 || exit 1;;
+    "set-led-state")
+        echo "set-led-state" $2 > $SERV_INPUT
+        cat $SERV_OUTPUT | grep FAILED && exit 1;;
+    *) echo "Unknown command"
+esac
+
