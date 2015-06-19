6ab5794f0ae216afb8d7d932cddb1652905c76c5
Dmitry Vyal <akamaus@gmail.com>
2015-06-19 13:23:44 +0300

    creating fifos on start

--- src/server.cpp
+++ src/server.cpp
@@ -20,13 +20,23 @@ constexpr int BUF_SIZE=1024;
 typedef FILE* FIFO;
 enum class FIFO_ROLE { Reader, Writer};
 
-FIFO open_fifo(const char *path, FIFO_ROLE role) {
-    if (access(path, F_OK ) != -1 ) {
+void create_fifo(const char *path) {
+    if (access(path, F_OK ) == -1 ) {
+        if (mkfifo(path, 0666) != 0) throw std::runtime_error(string("can't create fifo: ") + strerror(errno));
     } else {
-        if (mkfifo(path, 0666) != 0) throw std::runtime_error(strerror(errno));
+        struct stat s_res;
+        memset(&s_res, 0, sizeof(s_res));
+        if (stat(path, &s_res) == -1) {
+            throw std::runtime_error(string("can't stat fifo") + strerror(errno));
+        } else if (!S_ISFIFO(s_res.st_mode)) {
+            throw std::runtime_error(string("file exists but is not fifo: ") + path);
+        }
     }
+}
+
+FIFO open_fifo(const char *path, FIFO_ROLE role) {
     FILE *handle = fopen(path, role == FIFO_ROLE::Reader? "r":"w");
-    if (handle == nullptr) throw std::runtime_error(strerror(errno));
+    if (handle == nullptr) throw std::runtime_error(string("can't open fifo: ") + strerror(errno));
     return handle;
 }
 
@@ -53,6 +63,9 @@ Result interpret_line(char *line) {
 }
 
 int main(int argc, char *argv[]) {
+    create_fifo(INPUT_FIFO);
+    create_fifo(OUTPUT_FIFO);
+
     pthread_t thr;
     int res;
     res = pthread_create(&thr, nullptr, controller_loop, nullptr);
